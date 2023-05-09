f7b421dfac87ea9eac47eb5a8e6f23be7342068a
Caroline <carolineanjoss02@gmail.com>
2023-05-09 19:49:26 -0300

    creating new component and styles

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comments.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/carolineanjos.png" />
+      <Avatar hasBorder={false} src="https://github.com/carolineanjos.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar () {
   return (
@@ -7,7 +8,7 @@ export function Sidebar () {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50"/>
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/carolineanjos.png" />
+        <Avatar src="https://github.com/carolineanjos.png"/>
         <strong>Caroline Anjos</strong>
         <span>Web Developer</span>
       </div>
